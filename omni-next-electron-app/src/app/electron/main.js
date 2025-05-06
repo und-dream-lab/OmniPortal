2c027f4375834113e21d2138eeaee716ecb5515c
McKee <walker.mckee@ad.und.edu>
2025-05-06 10:45:41 -0500

    Created Next.js app, then added electron. To use, run: yarn run dev & yarn run start:electron

---
+++ omni-next-electron-app/src/app/electron/main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+const path = require('path')
+
+async function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  if (process.env.NODE_ENV === 'development') {
+    win.loadURL('http://localhost:3000')
+  } else {
+    win.loadFile(path.join(__dirname, '../out/index.html'))
+  }
+}
+
+app.whenReady().then(createWindow)
